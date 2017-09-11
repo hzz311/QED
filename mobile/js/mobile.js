ab4a4901a684e46fd1ec8a27b6ea1016115760ee
suntianhun <373887932@qq.com>
2017-09-11 12:17:15 +0800

    Signed-off-by: suntianhun <373887932@qq.com>

--- mobile/js/mobile.js
+++ mobile/js/mobile.js
@@ -1,3 +1,5 @@
+var isiOS = !!navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+
 //设置页面基本字体大小
 (function(){
 	var tref;
@@ -29,6 +31,87 @@
 		}
 	}, false);
 	setup();
+})();
+
+function transitionEnd(dom,callback) {
+	var events = ['webkitTransitionEnd', 'transitionend', 'oTransitionEnd', 'MSTransitionEnd', 'msTransitionEnd'], i, j, dom = dom;
+	function fireCallBack(e) {
+		if (e.target !== this) return;
+		callback.call(this, e);
+		for (i = 0; i < events.length; i++) {
+			dom.off(events[i], fireCallBack);
+		}
+	}
+	if (callback) {
+		for (i = 0; i < events.length; i++) {
+			dom.on(events[i], fireCallBack);
+		}
+	}
+	return this;
+}
+
+var Modal=(function(){
+
+	function createModal(htmlstr) {
+		var wrap=$('body');
+		var html='<div class="modal-overlay"></div><div class="modal">'+htmlstr+'</div>';
+		wrap.append(html);
+		var overlay =wrap.children('.modal-overlay');
+		var modal=wrap.children('.modal');
+		overlay.show();
+		modal.show();
+		overlay.addClass('modal-overlay-visible');
+		modal.addClass('modal-in');
+		overlay.on('click', function (e) {
+			e.preventDefault();
+			overlay.removeClass('modal-overlay-visible');
+			transitionEnd(overlay,function (e) {
+				overlay.remove();
+			});
+			modal.removeClass('modal-in').addClass('modal-out');
+			transitionEnd(modal,function (e) {
+				modal.remove();
+			});
+		});
+		return modal;
+	}
+
+	/**
+	 * msg
+	 */
+	function showTip(opts){
+		var wrapper=$('body');
+		var msg='',times=3000;
+		if(typeof opts === 'string'){
+			msg=opts;
+		}else{
+			msg=opts.msg;
+			times=opts.times||times;
+		}
+		wrapper.append('<div class="m-tipwinmask"><span class="m-tipwin">'+msg+'</span></div>');
+		var tipwin =wrapper.children('.m-tipwinmask:last');
+		tipwin.show();
+		tipwin.addClass('m-tipwinmask_in');
+
+		var hideTipAlert=function(){
+			tipwin.removeClass('m-tipwinmask_in');
+			transitionEnd(tipwin,function (e) {
+				tipwin.remove();
+			});
+		};
+		var tipshowTimeoutId=setTimeout(function(){
+			hideTipAlert();
+		},times);
+		tipwin.click(function(e){
+			e.preventDefault();
+			clearTimeout(tipshowTimeoutId);
+			hideTipAlert();
+		});
+	}
+	return {
+		show:createModal,
+		showTip:showTip
+	}
 })();
 
 //绑定导航事件
